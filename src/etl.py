405fb41887fc755c725ae5d0cb687d78b8b8e6e9
pankaj kumar <pankaj120099@gmail.com>
2026-04-24 03:05:10 -0700

    Converted notebook to python script

---
+++ src/etl.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import sqlite3
+
+
+# In[8]:
+
+
+# Extract
+df = pd.read_csv('../data/data101.csv')
+
+# Transform
+df['age'] = df['age'].fillna(0)
+df['salary'] = df['salary'] * 1.1
+
+# Load
+# conn = sqlite3.connect('test.db')
+# df.to_sql('users', conn, if_exists='replace', index=False)
+
+# print("ETL Completed")
+
+
+
+# In[9]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
