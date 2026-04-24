2cefcba4e27c9e9e8f14f0506851f9a09f65438a
pankaj kumar <pankaj120099@gmail.com>
2026-04-24 16:43:48 -0700

    Add Sql code

--- src/etl.py
+++ src/etl.py
@@ -8,7 +8,7 @@ import pandas as pd
 import sqlite3
 
 
-# In[8]:
+# In[13]:
 
 
 # Extract
@@ -18,17 +18,41 @@ df = pd.read_csv('data/data101.csv')
 df['age'] = df['age'].fillna(0)
 df['salary'] = df['salary'] * 1.1
 
+# Load
+# conn = sqlite3.connect('test.db')
+# df.to_sql('users', conn, if_exists='replace', index=False)
 
+print("ETL Completed")
+
+
+
+# In[9]:
+
+
+df
+
+
+# In[6]:
+
+
+# Load
 conn = sqlite3.connect('test.db')
 df.to_sql('users', conn, if_exists='replace', index=False)
-pd.read_sql()
-
-print("You  are inside Github Actions ETL Completed have fun inside ETL Using Github Action")
 
 
+# In[10]:
 
 
+res = pd.read_sql('Select * from users',con=conn)
 
+
+# In[12]:
+
+
+res
+
+
+# In[ ]:
 
 
 
